820d05e0aa9f7f8baba307ba64aef6da788a8907
2No2Name <2No2Name@web.de>
2022-01-15 21:54:41 +0100

    change: mod name and package names

--- src/main/java/_2no2name/miscanium/mixin/ExampleMixin.java
+++ src/main/java/_2no2name/miscanium/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package _2no2name.miscanium.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import _2no2name.miscanium.MiscaniumMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		MiscaniumMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
